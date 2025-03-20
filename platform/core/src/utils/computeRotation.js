4ab78203a9b5c1b1c04367e6a28e14f336d8ff98
Mo Alsad <mo.alsad@gmail.com>
2025-03-20 00:13:56 +0000

    Fix the rotation of the pathology slide preview in the thumbnail images

--- platform/core/src/utils/computeRotation.js
+++ platform/core/src/utils/computeRotation.js
@@ -5,14 +5,19 @@ const computeRotation = iop => {
 
   const rowDirection = iop.slice(0, 3);
   const columnDirection = iop.slice(3, 6);
-  const rotationMatrix = [
+  const mat = [
     [rowDirection[0], columnDirection[0]],
     [rowDirection[1], columnDirection[1]],
     [rowDirection[2], columnDirection[2]],
   ];
-  let angle = Math.atan2(-rotationMatrix[0][0], rotationMatrix[0][1]);
+  // sin(t), cos(t)
+  let angle = Math.atan2(-mat[0][1], mat[0][0]);
   // In degrees
-  angle = angle / (Math.PI / 180);
+  angle = angle * (180 / Math.PI);
+
+  // The default rotation is 'horizontal' with the slide label on the right
+  // Rotate clockwise by 90 degrees to have slide label at the top
+  angle += 90;
 
   return angle;
 };
